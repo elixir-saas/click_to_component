26b30d4f082a033d1548f5f1ca94b69990af7b47
Justin Tormey <justin@upsidefinancing.com>
2023-10-06 10:24:34 -0500

    Add esbuild dep

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule ClickToComponent.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      name: "Click To Component",
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -19,7 +21,15 @@ defmodule ClickToComponent.MixProject do
 
   defp deps do
     [
-      {:phoenix_live_view, "~> 0.20"}
+      {:phoenix_live_view, "~> 0.20"},
+      {:esbuild, "~> 0.2", only: :dev}
+    ]
+  end
+
+  defp aliases do
+    [
+      "assets.build": ["esbuild module", "esbuild cdn", "esbuild cdn_min", "esbuild main"],
+      "assets.watch": ["esbuild module --watch"]
     ]
   end
 end
