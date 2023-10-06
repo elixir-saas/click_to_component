156ceaf821d0c5cf2f6b875a5d63d2137cb34334
Justin Tormey <justin@upsidefinancing.com>
2023-10-06 10:55:04 -0500

    Fix priv/static paths

--- config/config.exs
+++ config/config.exs
@@ -7,7 +7,7 @@ config :logger, :backends, []
 if Mix.env() == :dev do
   esbuild = fn args ->
     [
-      args: ~w(./js/click_to_command --bundle) ++ args,
+      args: ~w(./js/click_to_component --bundle) ++ args,
       cd: Path.expand("../assets", __DIR__),
       env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
     ]
@@ -16,14 +16,15 @@ if Mix.env() == :dev do
   config :esbuild,
     version: "0.12.15",
     module:
-      esbuild.(~w(--format=esm --sourcemap --outfile=../priv/static/click_to_command.esm.js)),
-    main: esbuild.(~w(--format=cjs --sourcemap --outfile=../priv/static/click_to_command.cjs.js)),
+      esbuild.(~w(--format=esm --sourcemap --outfile=../priv/static/click_to_component.esm.js)),
+    main:
+      esbuild.(~w(--format=cjs --sourcemap --outfile=../priv/static/click_to_component.cjs.js)),
     cdn:
       esbuild.(
-        ~w(--format=iife --target=es2016 --global-name=LiveView --outfile=../priv/static/click_to_command.js)
+        ~w(--format=iife --target=es2016 --global-name=LiveView --outfile=../priv/static/click_to_component.js)
       ),
     cdn_min:
       esbuild.(
-        ~w(--format=iife --target=es2016 --global-name=LiveView --minify --outfile=../priv/static/click_to_command.min.js)
+        ~w(--format=iife --target=es2016 --global-name=LiveView --minify --outfile=../priv/static/click_to_component.min.js)
       )
 end
